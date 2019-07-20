4d43af95982ac2e38261c1f56464de3988f4b748
Zebbzz <tdcrites94@gmail.com>
2019-07-20 01:27:49 -0400

    gitignore issues

--- src/main/java/me/zebbzz/brotatobot/functionality/hosting/Secrets.java
+++
@@ -1,6 +0,0 @@
-package me.zebbzz.brotatobot.functionality.hosting;
-
-public class Secrets {
-    public static final String TOKEN = "NTk0MzkyMDI5MTU3NTIzNDU2.XTHn0Q.bvrANiZ-9N36uONFQsauesQEnaU";
-    //Gist Token: d5215bec2e693ca56eb4b75a879d96986a65538f
-}
