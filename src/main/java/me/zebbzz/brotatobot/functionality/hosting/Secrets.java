ffc3819091b5fe2769ff088ac0493f1286b56812
Zebbzz <tdcrites94@gmail.com>
2019-07-20 01:17:59 -0400

    Add gitignore. Removed bot token
    Prevent further bot token leaks...

--- src/main/java/me/zebbzz/brotatobot/functionality/hosting/Secrets.java
+++
@@ -1,6 +0,0 @@
-package me.zebbzz.brotatobot.functionality.hosting;
-
-public class Secrets {
-    public static final String TOKEN = "NTk0MzkyMDI5MTU3NTIzNDU2.XSuK3Q.W0wse_NnRQYj9X8Kha35xHNDC8I";
-    //Gist Token: d5215bec2e693ca56eb4b75a879d96986a65538f
-}
