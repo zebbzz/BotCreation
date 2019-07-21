58eb1c6d7f24ae6d9494c900d3f20cea545d0bec
Zebbzz <tdcrites94@gmail.com>
2019-07-21 03:14:37 -0400

    Refactor packages. CatCommand updated.

---
+++ src/main/java/brotatobot/commands/textcommands/HelpCommand.java
@@ -0,0 +1,68 @@
+package brotatobot.commands.textcommands;
+
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import brotatobot.functionality.CommandManager;
+import brotatobot.functionality.Constants;
+import brotatobot.objects.ICommand;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.List;
+
+public class HelpCommand implements ICommand {
+
+    private final CommandManager manager;
+
+    public HelpCommand(CommandManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent event) {
+
+        if (args.isEmpty()) {
+            generateAndSendEmbed(event);
+            return;
+        }
+
+        String joined = String.join("", args);
+
+        ICommand command = manager.getCommand(joined);
+
+        if (command == null) {
+            event.getChannel().sendMessage("The command `" + joined + "` does not exist\n" +
+                    "Use `" + Constants.PREFIX + getInvoke() + "` for a list of commands").queue();
+            return;
+        }
+
+        String message = "Command help for `" + command.getInvoke() + "`\n" + command.getHelp();
+
+        event.getChannel().sendMessage(message).queue();
+    }
+
+    private void generateAndSendEmbed(GuildMessageReceivedEvent event) {
+
+        EmbedBuilder builder = EmbedUtils.defaultEmbed().setTitle("A list of all my commands:");
+
+        StringBuilder descriptionBuilder = builder.getDescriptionBuilder();
+
+        manager.getCommands().forEach(
+                (command) -> descriptionBuilder.append('`').append(command.getInvoke()).append("`\n")
+        );
+
+        //TODO: Make a permission check to see if the bot can send embeds if not, send plain text
+        event.getChannel().sendMessage(builder.build()).queue();
+    }
+
+
+    @Override
+    public String getHelp() {
+        return "Shows a list of all the commands.\n" +
+                "Usage: `" + Constants.PREFIX + getInvoke() + " [command]`";
+    }
+
+    @Override
+    public String getInvoke() {
+        return "help";
+    }
+}
---
+++ src/main/java/brotatobot/functionality/Main.java
@@ -0,0 +1,66 @@
+package brotatobot.functionality;
+
+import brotatobot.functionality.hosting.Secrets;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import me.duncte123.botcommons.web.WebUtils;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.time.Instant;
+import java.util.Random;
+
+/** Text in here **/
+
+public class Main {
+
+    private final Random random = new Random();
+
+
+    private Main() {
+
+        CommandManager commandManager = new CommandManager();
+        Listener listener = new Listener(commandManager);
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        WebUtils.setUserAgent("Mozilla/5.0 Brotato/Zebbzz#6235");
+        EmbedUtils.setEmbedBuilder(
+                () -> new EmbedBuilder()
+                        .setColor(getRandomColor())
+                        .setFooter("Developed by Zebbzz", "https://discord.gg/CXksb8u")
+                        .setTimestamp(Instant.now())
+        );
+
+        try {
+            logger.info("Booting");
+            new JDABuilder(AccountType.BOT) //Sets account type as bot
+                    .setToken(Secrets.TOKEN) // Token stored in Secrets class, logs in to the bot
+                    .setAudioEnabled(false) // <-- Read
+                    .setGame(Game.streaming("Join Our Discord!", "https://discord.gg/CXksb8u")) // <- Discord invite link stored as a "Playing..."
+                    .addEventListener(listener) //Read listener class and execute code inside
+                    .build().awaitReady(); //
+            logger.info("Running");
+        } catch (LoginException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Color getRandomColor() {
+        float r = random.nextFloat();
+        float g = random.nextFloat();
+        float b = random.nextFloat();
+
+        return new Color(r, g, b);
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
