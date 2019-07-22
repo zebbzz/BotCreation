840e701112541ca845e6b48d84ef87b2e146277d
Zebbzz <tdcrites94@gmail.com>
2019-07-22 13:57:29 -0400

    Added new commands
    Co-Authored-By: Mooaz Nasir <mooaz09@gmail.com>

--- src/main/java/brotatobot/functionality/Constants.java
+++ src/main/java/brotatobot/functionality/Constants.java
@@ -1,6 +1,10 @@
 package brotatobot.functionality;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
     public static final String PREFIX = "?";
     public static final long OWNER = 170325189337284609L;
+    public static final Map<Long, String> PREFIXES = new HashMap<>();
 }
